e4cc5013affa63e63e4a708911a403735b2df5b6
punkername007 <152019449+punkername007@users.noreply.github.com>
2023-12-11 20:26:03 +0100

    get data from API and update infomation

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-function search(event) {
+function handleSearch(event) {
   event.preventDefault();
   let userInput = document.querySelector("#search-field");
   let apiKey = "e2c0b68bt1bfbc04o7da0f6ea7720334";
-
-  let url = `https://api.shecodes.io/weather/v1/current?query=miami&key=${apiKey}`;
+  let city = userInput.value;
+  let url = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
   axios.get(url).then(showData);
 }
 
@@ -14,6 +14,15 @@ function showData(response) {
 
   let city = document.querySelector("#current-city");
   city.innerHTML = `${response.data.city}`;
+
+  console.log(response);
+
+  let condition = document.querySelector("#condition");
+  condition.innerHTML = `${response.data.condition.description}`;
+  let humidity = document.querySelector("#humidity");
+  humidity.innerHTML = `${response.data.temperature.humidity}`;
+  let wind = document.querySelector("#wind");
+  wind.innerHTML = `${response.data.wind.speed}km/h`;
 }
 
 function formatDate(data) {
@@ -43,7 +52,7 @@ function formatDate(data) {
 }
 
 let searchForm = document.querySelector("#search-form");
-searchForm.addEventListener("submit", search);
+searchForm.addEventListener("submit", handleSearch);
 
 let currentDay = new Date();
 let data = document.querySelector("#current-data");
